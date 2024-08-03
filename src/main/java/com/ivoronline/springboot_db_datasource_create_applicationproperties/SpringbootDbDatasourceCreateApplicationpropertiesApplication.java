2f0fd30645f6c11e4165b040bac0540cf1eed8c3
ivoronline <ivoronline@gmail.com>
2024-08-03 11:00:42 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_db_datasource_create_applicationproperties/SpringbootDbDatasourceCreateApplicationpropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_db_datasource_create_applicationproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDbDatasourceCreateApplicationpropertiesApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootDbDatasourceCreateApplicationpropertiesApplication.class, args);
+  }
+  
+}
---
+++ src/test/java/com/ivoronline/springboot_db_datasource_create_applicationproperties/SpringbootDbDatasourceCreateApplicationpropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_db_datasource_create_applicationproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDbDatasourceCreateApplicationpropertiesApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
